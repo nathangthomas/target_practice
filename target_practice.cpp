f0fcd6f9bec8779c6159c0bce8e0b8ba925b70f1
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-05-05 23:34:33 -0600

    Add iteration two code

--- target_practice.cpp
+++ target_practice.cpp
@@ -7,6 +7,9 @@ int miss_button = 8;
 int hit_count = 0;
 int miss_count = 0;
 int total_shots = 0;
+int Green = 6;
+int Red = 9;
+int White = 10;
 
 void setup() {
   // set up the LCD's number of columns and rows:
@@ -21,34 +24,102 @@ void setup() {
   lcd.print("Target Practice");
   delay(4000);
 
-  lcd.setCursor(1,0);
-  lcd.print(String("Hits: ") + String(hit_count) + String("          "));
-  lcd.setCursor(1,1);
-  lcd.print(String("Total Shots: ") + String(total_shots) + String("          "));
+ lcd.setCursor(1,0);
+ lcd.print(String("Hits: ") + String(hit_count) + String("          "));
+ lcd.setCursor(1,1);
+ lcd.print(String("Total Shots: ") + String(total_shots) + String("          "));
+
+// controls leds
+ Serial.begin(9600);
+ pinMode(Green, OUTPUT);
+ digitalWrite(Green, LOW);
+ pinMode(Red, OUTPUT);
+ digitalWrite(Red, LOW);
+ pinMode(White, OUTPUT);
+ digitalWrite(White, LOW);
 
 }
 
 void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
-  //  To be used in future iterations for timed target practice
-  // print the number of seconds since reset:
-  //  lcd.print(millis() / 1000);
+//  To be used in future iterations for timed target practice
+// print the number of seconds since reset:
+//  lcd.print(millis() / 1000);
 
-    if (digitalRead(hit_button) == HIGH){
-      hit_count++;
-      total_shots = hit_count + miss_count;
+//    if (count == 0){
+//      lcd.print(String("Target Practice"));
+//    }
+//
 
+  if (digitalRead(hit_button) == LOW && digitalRead(miss_button) == LOW){
+    digitalWrite(White, HIGH);
+  } else {
+    digitalWrite(White, LOW);
+  }
 
-      lcd.setCursor(1,0);
-      lcd.print(String("Hits: ") + String(hit_count) + String("          "));
-      lcd.setCursor(1,1);
-      lcd.print(String("Total Shots: ") + String(total_shots) + String("          "));
+  if (digitalRead(hit_button) == HIGH){
+    hit_count++;
+    total_shots = hit_count + miss_count;
+
+     lcd.setCursor(1,0);
+     lcd.print(String("Hits: ") + String(hit_count) + String("          "));
+     lcd.setCursor(1,1);
+     lcd.print(String("Total Shots: ") + String(total_shots) + String("          "));
+
+      digitalWrite(Green, HIGH);
+      delay(100);
+      digitalWrite(Green, LOW);
+      delay(100);
+      digitalWrite(Green, HIGH);
+      delay(100);
+      digitalWrite(Green, LOW);
+      delay(100);
+      digitalWrite(Green, HIGH);
+      delay(100);
+      digitalWrite(Green, LOW);
+      delay(100);
+      digitalWrite(Green, HIGH);
+      delay(500);
+      digitalWrite(Green, LOW);
+      delay(100);
+      digitalWrite(Green, HIGH);
+      delay(100);
+      digitalWrite(Green, LOW);
+      delay(100);
+      digitalWrite(Green, HIGH);
+      delay(100);
+      digitalWrite(Green, LOW);
+      delay(100);
+      digitalWrite(Green, HIGH);
+      delay(100);
+      digitalWrite(Green, LOW);
+      delay(100);
+      digitalWrite(Green, HIGH);
+      delay(500);
+      digitalWrite(Green, LOW);
+      delay(100);
+      digitalWrite(Green, HIGH);
+      delay(100);
+      digitalWrite(Green, LOW);
+      delay(100);
+      digitalWrite(Green, HIGH);
+      delay(100);
+      digitalWrite(Green, LOW);
+      delay(100);
+      digitalWrite(Green, HIGH);
+      delay(100);
+      digitalWrite(Green, LOW);
+      delay(100);
+      digitalWrite(Green, HIGH);
+      delay(1000);
+      digitalWrite(Green, LOW);
 
       while(digitalRead(hit_button)== HIGH);
-        delay(2000);
-    }
+  }
 
-    if (digitalRead(miss_button) == HIGH){
+   if (digitalRead(miss_button) == HIGH){
       lcd.setCursor(0,0);
       miss_count++;
       total_shots = hit_count + miss_count;
@@ -57,9 +128,28 @@ void loop() {
       lcd.print(String("Hits: ") + String(hit_count) + String("          "));
       lcd.setCursor(1,1);
       lcd.print(String("Total Shots: ") + String(total_shots) + String("          "));
-     
+
+      digitalWrite(Red, HIGH);
+      delay(1000);
+      digitalWrite(Red, LOW);
+      delay(100);
+      digitalWrite(Red, HIGH);
+      delay(1000);
+      digitalWrite(Red, LOW);
+      delay(100);
+      digitalWrite(Red, HIGH);
+      delay(1000);
+      digitalWrite(Red, LOW);
+      delay(100);
+      digitalWrite(Red, HIGH);
+      delay(100);
+      digitalWrite(Red, LOW);
+      delay(100);
+      digitalWrite(Red, HIGH);
+      delay(1000);
+      digitalWrite(Red, LOW);
+
       while(digitalRead(miss_button)== HIGH);
-      delay(2000);
     }
 
 }
